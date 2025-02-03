67d1b163168080e4438eba00256bd7be666c2d61
sergeykochiev <sergeyivanovo547@gmail.com>
2025-02-03 08:21:55 +0300

    updated build script

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-gcc main.c -I%userprofile%\gcc\x86_64-w64-mingw32\include -L%userprofile%\gcc\x86_64-w64-mingw32\lib -luser32 -lgdi32 -o main
+gcc main.c -I%userprofile%\gcc\x86_64-w64-mingw32\include -L%userprofile%\gcc\x86_64-w64-mingw32\lib -luser32 -lgdi32 -lntdll -lshcore -o main
